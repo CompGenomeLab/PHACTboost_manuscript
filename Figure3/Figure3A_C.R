68b693ecffe366f0ac46e19fd452cad95d5088e1
Nurdan Kuru <68369488+nurdannkuru@users.noreply.github.com>
2023-10-30 18:48:10 +0300

    Update Figure3A_C.R

--- Figure3/Figure3A_C.R
+++ Figure3/Figure3A_C.R
@@ -1,8 +1,8 @@
-ts1 <- read.csv("Data/Figure1_TS1.csv")
-ts2 <- read.csv("Data/Figure1_TS2.csv")
-ts3 <- read.csv("Data//Figure1_TS3.csv")
-ts4 <- read.csv("Data//Figure1_TS4.csv")
-ts5 <- read.csv("Data//Figure1_TS5.csv")
+ts1 <- read.csv("Data/Figure3_TS1.csv")
+ts2 <- read.csv("Data/Figure3_TS2.csv")
+ts3 <- read.csv("Data/Figure3_TS3.csv")
+ts4 <- read.csv("Data/Figure3_TS4.csv")
+ts5 <- read.csv("Data/Figure3_TS5.csv")
 
 all_sets <- list()
 all_sets$ts1 <- ts1
@@ -83,8 +83,8 @@ values_roc_alpmis <- as.data.frame(values_roc_alpmis)
 values_pr_alpmis <- as.data.frame(values_pr_alpmis)
 values_roc_eve <- as.data.frame(values_roc_eve)
 values_pr_eve <- as.data.frame(values_pr_eve)
-values_roc_cpt <- as.data.frame(values_roc_cpt)
-values_pr_cpt <- as.data.frame(values_pr_cpt)
+values_roc_cpt1 <- as.data.frame(values_roc_cpt1)
+values_pr_cpt1 <- as.data.frame(values_pr_cpt1)
 
 rownames(values_roc_alpmis) <- c("Neutral", "Pathogenic", "PHACTboost", "AlphaMissense")
 rownames(values_pr_alpmis) <- c("Neutral", "Pathogenic", "PHACTboost", "AlphaMissense")
@@ -94,15 +94,23 @@ rownames(values_roc_cpt1) <- c("Neutral", "Pathogenic", "PHACTboost", "CPT1")
 rownames(values_pr_cpt1) <- c("Neutral", "Pathogenic", "PHACTboost", "CPT1")
 
 
+dataset_names <- c("TS1", "TS2", "TS3", "TS4", "TS5")
+colnames(values_roc_alpmis) <- dataset_names
+colnames(values_pr_alpmis) <- dataset_names
+colnames(values_roc_eve) <- dataset_names
+colnames(values_pr_eve) <- dataset_names
+colnames(values_roc_cpt1) <- dataset_names
+colnames(values_pr_cpt1) <- dataset_names
+
 cols <- c("#18401E", "#A9D9A7")
-pdf(file = "EVE_ROC.pdf", width = 9, height = 5)
+pdf(file = "AlphaMissense_ROC.pdf", width = 9, height = 5)
 barplot(as.matrix(values_roc_alpmis[3:4,]), col = cols, beside = TRUE, ylim = c(0,1.3), yaxt = "n")
 legend(12, 1.25, legend = rownames(values_roc_alpmis[3:4,]), fill = cols, bty = "n", y.intersp = 0.7)
 axis(side = 2, at = seq(0,1,0.2), labels = sprintf("%.2f",seq(0,1,0.2)), las = 2)
 mtext(side = 2, text = "AUROC", line = 3, cex = 1)
 dev.off()
 
-pdf(file = "EVE_PR.pdf", width = 9, height = 5)
+pdf(file = "AlphaMissense_PR.pdf", width = 9, height = 5)
 barplot(as.matrix(values_pr_alpmis[3:4,]), col = cols, beside = TRUE, ylim = c(0,1.3), yaxt = "n")
 legend(12, 1.25, legend = rownames(values_pr_alpmis[3:4,]), fill = cols, bty = "n", y.intersp = 0.7)
 axis(side = 2, at = seq(0,1,0.2), labels = sprintf("%.2f",seq(0,1,0.2)), las = 2)
@@ -124,17 +132,18 @@ axis(side = 2, at = seq(0,1,0.2), labels = sprintf("%.2f",seq(0,1,0.2)), las = 2
 mtext(side = 2, text = "AUPR", line = 3, cex = 1)
 dev.off()
 
-cols <- c("#18401E", "#A9D9A7")
+cols <- c("#18401E", "#F2B694")
 pdf(file = "CPT1_ROC.pdf", width = 9, height = 5)
-barplot(as.matrix(values_roc_cpt[3:4,]), col = cols, beside = TRUE, ylim = c(0,1.3), yaxt = "n")
-legend(12, 1.25, legend = rownames(values_roc_cpt[3:4,]), fill = cols, bty = "n", y.intersp = 0.7)
+barplot(as.matrix(values_roc_cpt1[3:4,]), col = cols, beside = TRUE, ylim = c(0,1.3), yaxt = "n")
+legend(12, 1.25, legend = rownames(values_roc_cpt1[3:4,]), fill = cols, bty = "n", y.intersp = 0.7)
 axis(side = 2, at = seq(0,1,0.2), labels = sprintf("%.2f",seq(0,1,0.2)), las = 2)
 mtext(side = 2, text = "AUROC", line = 3, cex = 1)
 dev.off()
 
 pdf(file = "CPT1_PR.pdf", width = 9, height = 5)
-barplot(as.matrix(values_pr_cpt[3:4,]), col = cols, beside = TRUE, ylim = c(0,1.3), yaxt = "n")
-legend(12, 1.25, legend = rownames(values_pr_cpt[3:4,]), fill = cols, bty = "n", y.intersp = 0.7)
+barplot(as.matrix(values_pr_cpt1[3:4,]), col = cols, beside = TRUE, ylim = c(0,1.3), yaxt = "n")
+legend(12, 1.25, legend = rownames(values_pr_cpt1[3:4,]), fill = cols, bty = "n", y.intersp = 0.7)
 axis(side = 2, at = seq(0,1,0.2), labels = sprintf("%.2f",seq(0,1,0.2)), las = 2)
 mtext(side = 2, text = "AUPR", line = 3, cex = 1)
 dev.off()
+
